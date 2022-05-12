4973cebfd3a441bb827fbeabc8836f751bf860d0
Zen Huzaini <huzainimhd@gmail.com>
2022-05-12 13:41:21 +0200

    add heroku setting

--- src/modules/item/item.service.ts
+++ src/modules/item/item.service.ts
@@ -48,7 +48,6 @@ export class ItemService {
   //Scehduling every midninght, the rate must be updated
   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
   handleSchedule() {
-    console.log('ting ');
     this.rate = this.httpService
       .get('http://api.nbp.pl/api/exchangerates/tables/C/today/')
       .toPromise()
